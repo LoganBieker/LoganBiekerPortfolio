6faa71a92a0f43ab324f78fd74046d26451430ce
LoganBieker <133885615+LoganBieker@users.noreply.github.com>
2024-09-05 21:28:13 -0400

    yml update

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',         // Specify the output directory
+    assetsDir: 'assets',     // Directory to nest generated assets under
+  },
+  base: '/',                // Adjust this if your app is deployed under a subpath (e.g., '/app/')
+});
