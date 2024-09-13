5b3cf8df4a3e840655328e196df247de634dfdaa
LoganBieker <133885615+LoganBieker@users.noreply.github.com>
2024-09-13 11:47:03 -0400

    added demos to project

--- portfolio-website/src/Pages/Brewing.jsx
+++ portfolio-website/src/Pages/Brewing.jsx
@@ -16,8 +16,11 @@ export default function Brewing()
             <p>I developed this website to showcase my homebrewing projects, which I pursue in my free time. The site is designed following the Model-View-Controller (MVC) architecture, with a React-based front end and a Node.js and Express back end. To ensure scalability, both the front and back ends are containerized, and the entire application is currently hosted on Microsoft Azure. </p> 
             <p>The <span>Location</span> tab utilizes Leaflet, an open-source mapping tool. In the <span>Contact Us</span> section, users can submit feedback, which is stored as a file on the server. The <span>Brews</span> section provides access to brewing data, which is stored in an MSSQL database hosted on Azure. I initially used MySQL but switched to MSSQL due to better support from Azure. The <span>Events</span> tab manages JSON event files. The backend parses these files and automatically updates event dates and times for recurring events, which can be set on a daily, weekly, or monthly basis. For the website design, I intentionally avoided using component frameworks or styling libraries to deepen my understanding of React and to improve my CSS styling skills.</p>
             <p>You can view the website <a href={washbearUrl}>Here.</a> Please note that the backend may take a few minutes to start up, as it is hosted on a free plan and requires time for the containers to initialize.</p>
+             <iframe className="video" src="https://www.youtube.com/embed/cEibvZjDjU8"></iframe> 
         </div>
         </div>
         </>
+
+
     );
 }
--- portfolio-website/src/Pages/Weather.jsx
+++ portfolio-website/src/Pages/Weather.jsx
@@ -12,6 +12,8 @@ export default function Weather() {
                     <p>To practice using RESTful API patterns and C#, I developed a weather app that provides a 48-hour forecast based on the user's location. The front end is built using React, and for location input, I integrated the Google Maps API to offer location suggestions as the user types. Once a location is selected and the submit button is pressed, the coordinates (latitude and longitude) are sent to the backend via a fetch request.</p>
                     <p>The backend processes these coordinates by sending them to the OpenWeather API, which retrieves the weather data for the next 72 hours, including temperature, precipitation amount, and chance of precipitation. The backend filters out unnecessary information, parses the relevant data, and sends it back to the front end for display.</p>
                     <p>This project follows a client-server architecture utilizing RESTful API principles.</p>
+                    <iframe className="video" src="https://www.youtube.com/embed/dhXynevLuMs "></iframe> 
+
                 </div>
             </div>
         </>
