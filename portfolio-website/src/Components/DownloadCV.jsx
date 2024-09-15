99144d9404184b45e59ae283cc7c485deb10a674
LoganBieker <133885615+LoganBieker@users.noreply.github.com>
2024-09-15 18:11:35 -0400

    udpated path

--- portfolio-website/src/Components/DownloadCV.jsx
+++ portfolio-website/src/Components/DownloadCV.jsx
@@ -3,7 +3,8 @@ import './DownloadCV.css';
 export default function DownloadCV()
 {
 
-    const url = "https://drive.google.com/file/d/1-ya99EVLdBsV0YsIEGjx5hWl4u0CKE_V/view"
+
+    const url = "https://drive.google.com/file/d/1-ya99EVLdBsV0YsIEGjx5hWl4u0CKE_V/view?usp=sharing" 
     return (
             <a className="download-link" href={url} target="_blank">
             <div className="download-button" >Download<br />CV</div>
